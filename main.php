35a493a4308ff446d5fd435a6417d6cdf4281523
Nevena Marjanovic <marjanovicnevena18@gmail.com>
2022-01-26 16:30:09 +0100

    Inicijalizacija repozitorijuma

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+$name ="Krojac";
+if (condition) {
+  // code...
+}
+if (condition) {
+  // code...
+} else {
+  // code...
+}
+$db = my
+
+ ?>
---
+++ main2.js
@@ -0,0 +1,12 @@
+console.log(username);
+console.log();
+function info() {
+  if (true) {
+
+  }
+  if (true) {
+
+  } else {
+
+  }
+}
